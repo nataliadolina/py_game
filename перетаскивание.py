4e96689a7ebf6091fea5fdc630db45c46132593d
Natalia <nat.dol30@mail.ru>
2019-03-05 17:45:26 +0300

    -- Перетаскивание

--- перетаскивание.py
+++ перетаскивание.py
@@ -7,9 +7,13 @@ size = width, height = 300, 300
 screen = pygame.display.set_mode(size)
 running = True
 flag = False
-p = None
+flag1 = False
+x1, y1 = 0, 0
+pygame.draw.rect(screen, (0, 255, 0), [x1, y1, 100, 100])
+pygame.display.flip()
+x, y = None, None
 while running:
-    screen.fill((0, 0, 255))
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
@@ -18,4 +22,16 @@ while running:
         elif event.type == pygame.MOUSEBUTTONUP:
             flag = False
         elif event.type == pygame.MOUSEMOTION:
-            pos = event.pos
+            if flag:
+                x, y = event.pos
+    if x is not None and y is not None:
+        if (x >= x1 and x <= x1 + 100) and (y >= y1 and y <= y1 + 100):
+            delta_x, delta_y = x - x1, y - y1
+            pygame.draw.rect(screen, (0, 255, 0), [x - delta_x, y + delta_y, 100, 100])
+            x1, y1 = x, y
+            flag1 = True
+    if not flag1:
+        pygame.draw.rect(screen, (0, 255, 0), [x1, y1, 100, 100])
+    flag1 = False
+    clock.tick(FPS)
+    pygame.display.flip()
